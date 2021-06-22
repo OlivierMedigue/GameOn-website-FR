38d73ffab559f897bf927691db14b90c71872c0b
OlivierMedigue <olivier.medigue@expleogroup.com>
2021-06-22 15:05:04 +0200

    add lastname

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,7 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCross = document.querySelector(".close");
 
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -25,6 +26,43 @@ modalCross.addEventListener("click", closeModal);
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
-  console.log("pop-up fermé")
+  //console.log("pop-up fermé")
 }
 
+//-----first Name-----
+let firstName = validationFirst; 
+document.getElementById("first").addEventListener("change", validationFirst);
+function validationFirst(){
+  let firstName = document.getElementById("first").value;  
+  if (firstName !== null && firstName.length > 1) {// First Valid
+    console.log('le prenom est correct');
+  } else {// First InValid
+    console.log('le prenom est incorrect');
+  }
+}
+//-----last Name-----
+let lastName = validationLast; 
+document.getElementById("last").addEventListener("change", validationLast);
+function validationLast(){
+  let lastName = document.getElementById("last").value;  
+  if (lastName !== null && lastName.length > 1) {// First Valid
+    console.log('le nom est correct');
+  } else {// First InValid
+    console.log('le nom est incorrect');
+  }
+}
+//-----emailAdress-----
+let email = ValidateEmail;
+document.getElementById("email").addEventListener("change", ValidateEmail);
+function ValidateEmail(inputText)
+{
+var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+if(inputText.value(mailformat))
+{
+console.log('OK');
+}
+else
+{
+console.log('KO');
+}
+}
