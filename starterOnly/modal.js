eb5033033c7750cb69d60787106a8f3c128c28cd
OlivierMedigue <olivier.medigue@expleogroup.com>
2021-07-16 11:04:06 +0200

    clean js script

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -18,7 +18,7 @@ const closeCross = document.querySelector(".modal-valid .close");
 
 let errorMessages = {
   firstLastName: "Veuillez entrer 2 caractères ou plus",
-  email: "Veuillez enter une adresse mail valide",
+  email: "Veuillez entrer une adresse mail valide",
   birthdate: "Veuillez indiquer une date de naissance",
   quantity: "Veuillez indiquer un nombre entre 0 et 99",
   location: "Veuillez sélectionner une ville",
@@ -42,154 +42,170 @@ modalCross.addEventListener("click", function(){
   //-----first Name-----
 
 let isFirstNameCorrect = false;
-document.getElementById("first").addEventListener("input", function(){
-  let firstName = document.getElementById("first");  
+document.getElementById("first").addEventListener("input", checkFirstName)
+function checkFirstName(){
+  let firstName = document.getElementById("first")  
   if (firstName.value !== null && firstName.value.length > 1) {
     //console.log('le prenom est correct');
-    firstName.parentNode.removeAttribute("data-error");
-    firstName.parentNode.removeAttribute("data-error-visible");
-    isFirstNameCorrect=true;
-  }else {
+    firstName.parentNode.removeAttribute("data-error")
+    firstName.parentNode.removeAttribute("data-error-visible")
+    isFirstNameCorrect = true
+  } else {
     //console.log('le prenom est incorrect');
-    firstName.parentNode.setAttribute("data-error", errorMessages.firstLastName);
-    firstName.parentNode.setAttribute("data-error-visible", "true");
+    firstName.parentNode.setAttribute("data-error", errorMessages.firstLastName)
+    firstName.parentNode.setAttribute("data-error-visible", "true")
+    isFirstNameCorrect = false
   }
-})
+}
 
 //-----last Name-----
-let isLastNameCorrect = false;
-document.getElementById("last").addEventListener("input", function(){ 
-  let lastName = document.getElementById("last");  
+let isLastNameCorrect = false
+document.getElementById("last").addEventListener("input", checkLastName)
+function checkLastName(){ 
+  let lastName = document.getElementById("last")  
   if (lastName.value !== null && lastName.value.length > 1) {
     // console.log('le nom est correct');
-    isLastNameCorrect=true;
-    lastName.parentNode.removeAttribute("data-error");
-    lastName.parentNode.removeAttribute("data-error-visible");
+    isLastNameCorrect = true
+    lastName.parentNode.removeAttribute("data-error")
+    lastName.parentNode.removeAttribute("data-error-visible")
   } else {
     // console.log('le nom est incorrect');
-    lastName.parentNode.setAttribute("data-error", errorMessages.firstLastName);
-    lastName.parentNode.setAttribute("data-error-visible", "true");   
+    lastName.parentNode.setAttribute("data-error", errorMessages.firstLastName)
+    lastName.parentNode.setAttribute("data-error-visible", "true")
+    isLastNameCorrect = false   
   }
-})
+}
 
 //-----emailAdress-----
 
-let isEmailCorrect = false;
-document.getElementById("email").addEventListener("input", function(){
-  let email = document.getElementById("email");
-  const re = /^[a-z\d_\-]+(\.[\a-z\d\-]+)*@[a-z\d\-]+(\.[a-z\d]+)+$/;
+let isEmailCorrect = false
+document.getElementById("email").addEventListener("input", checkEmail)
+function checkEmail(){
+  let email = document.getElementById("email")
+  const re = /^[a-z\d_\-]+(\.[\a-z\d\-]+)*@[a-z\d\-]+(\.[a-z\d]+)+$/
   if (re.test(email.value) === true) {
     // console.log("L'email entré est bon")
-    isEmailCorrect=true;
-    email.parentNode.removeAttribute("data-error");
-    email.parentNode.removeAttribute("data-error-visible");
+    isEmailCorrect = true
+    email.parentNode.removeAttribute("data-error")
+    email.parentNode.removeAttribute("data-error-visible")
   } else {
     // console.log("L'email entré n'est pas bon")
-    email.parentNode.setAttribute("data-error", errorMessages.email);
-    email.parentNode.setAttribute("data-error-visible", "true");   
+    email.parentNode.setAttribute("data-error", errorMessages.email)
+    email.parentNode.setAttribute("data-error-visible", "true")
+    isEmailCorrect = false
   }
-})
+}
 
 //-----Birthdate-----
 
-let isBirthdateCorrect = false;
-document.getElementById("birthdate").addEventListener("input", function(){
-  let ddmmyy = document.getElementById("birthdate");
+let isBirthdateCorrect = false
+document.getElementById("birthdate").addEventListener("input",checkBirthdate)
+function checkBirthdate(){
+  let ddmmyy = document.getElementById("birthdate")
   if (ddmmyy.value !== "") {
     // console.log('la date est valide');
-    isBirthdateCorrect=true;
-    ddmmyy.parentNode.removeAttribute("data-error");
-    ddmmyy.parentNode.removeAttribute("data-error-visible");
+    isBirthdateCorrect = true
+    ddmmyy.parentNode.removeAttribute("data-error")
+    ddmmyy.parentNode.removeAttribute("data-error-visible")
   } else {
     // console.log('le date est invalide');
-    ddmmyy.parentNode.setAttribute("data-error", errorMessages.birthdate);
-    ddmmyy.parentNode.setAttribute("data-error-visible", "true");  
+    ddmmyy.parentNode.setAttribute("data-error", errorMessages.birthdate)
+    ddmmyy.parentNode.setAttribute("data-error-visible", "true")
+    isBirthdateCorrect = false
   }
-})
+}
 
 //-----quantity-----
 
-let isQuantityCorrect = false;
-document.getElementById("quantity").addEventListener("input", function(){ 
-  let myField = document.getElementById("quantity");
-  if( myField.value >= 0 && myField.value <= 99 && myField.value != "")  {
+let isQuantityCorrect = false
+document.getElementById("quantity").addEventListener("input", checkQuantity)
+function checkQuantity(){ 
+  let myField = document.getElementById("quantity")
+  if ( myField.value >= 0 && myField.value <= 99 && myField.value != "")  {
     //  console.log("quantité OK"); 
-     isQuantityCorrect=true;
-     myField.parentNode.removeAttribute("data-error");
-     myField.parentNode.removeAttribute("data-error-visible");
-  }
-  else{
+     isQuantityCorrect = true
+     myField.parentNode.removeAttribute("data-error")
+     myField.parentNode.removeAttribute("data-error-visible")
+  } else {
     // console.log("quantité KO");
-    myField.parentNode.setAttribute("data-error", errorMessages.quantity);
-    myField.parentNode.setAttribute("data-error-visible", "true");    
-  }     
-})
+    myField.parentNode.setAttribute("data-error", errorMessages.quantity)
+    myField.parentNode.setAttribute("data-error-visible", "true")
+    isQuantityCorrect = false
+  }    
+}
 
 //-----location------
 
-let city = document.getElementsByClassName("location");
 let isLocationCorrect = false;
-for(var i = 0; i < city.length; i++){  
-  var locations = city[i];
-  locations.addEventListener("click", function(){
-    if(locations.checked){
-      // console.log("ville selectionnee"); 
-      isLocationCorrect = true;
-      locations.parentNode.removeAttribute("data-error");
-      locations.parentNode.removeAttribute("data-error-visible");
-    } else{
-      // console.log("ville non selectionnee");
-      locations.parentNode.setAttribute("data-error", errorMessages.location);
-      locations.parentNode.setAttribute("data-error-visible", "true");
-    }  
-  })
+let locations = document.querySelectorAll("input[name=location]")
+for(var i = 0; i < locations.length; i++){
+ locations[i].addEventListener("click", checkLocations)    
+}
+function checkLocations(){
+  let locations = document.querySelectorAll("input[name=location]")
+  for(var i = 0; i < locations.length; i++) {   
+    if (locations[i].checked) {      
+      locations[i].parentNode.removeAttribute("data-error") 
+      locations[i].parentNode.removeAttribute("data-error-visible")
+      isLocationCorrect = true
+      break
+    } else {
+      locations[i].parentNode.setAttribute("data-error", errorMessages.location)
+      locations[i].parentNode.setAttribute("data-error-visible", "true")
+      isLocationCorrect = false
+    }
+  }
 }
 
+
 //-----Checkbox------
-let validatedBox = document.getElementById("checkbox1");
-let isValidatedBoxCorrect= false;
-validatedBox.addEventListener("click", function(){
+let validatedBox = document.getElementById("checkbox1")
+let isValidatedBoxCorrect = false
+validatedBox.addEventListener("click", checkValidedBox)
+function checkValidedBox(){
   if (validatedBox.checked){
-    // console.log("condition validee");
-    isValidatedBoxCorrect=true; 
-    validatedBox.parentNode.removeAttribute("data-error");
-    validatedBox.parentNode.removeAttribute("data-error-visible");
+    // console.log("condition validee")
+    isValidatedBoxCorrect = true 
+    validatedBox.parentNode.removeAttribute("data-error")
+    validatedBox.parentNode.removeAttribute("data-error-visible")
   } else {
     // console.log("condition non validee");
-    validatedBox.parentNode.setAttribute("data-error", errorMessages.condition);
-    validatedBox.parentNode.setAttribute("data-error-visible", "true");   
+    validatedBox.parentNode.setAttribute("data-error", errorMessages.condition)
+    validatedBox.parentNode.setAttribute("data-error-visible", "true")
+    isValidatedBoxCorrect = false 
   }
-})
+}
 
 
 function checkForm(){
-  let myFormData = new FormData(document.getElementById("my-form"))
-  myFormData = false;
-  if (isFirstNameCorrect == false || isLastNameCorrect == false || isEmailCorrect == false || isBirthdateCorrect == false || isQuantityCorrect == false || isValidatedBoxCorrect == false) {
-    return false;
-
-
-  // }else if (isLocationCorrect == false){
-  
+  checkFirstName()
+  checkLastName ()
+  checkEmail ()
+  checkBirthdate()
+  checkQuantity ()
+  checkLocations ()
+  checkValidedBox()
+  if (isFirstNameCorrect == false || isLastNameCorrect == false || isEmailCorrect == false || isBirthdateCorrect == false || isQuantityCorrect == false || isLocationCorrect == false || isValidatedBoxCorrect == false) {
+    return false
   }else
-    return true;
-  }
-
+    return true
+}
 
 document.getElementById("submit-button").addEventListener("click", function (e){
-  e.preventDefault();
+  e.preventDefault()
   if (checkForm()){
-    validationPopUp.style.display = "block";
-    modalbg.style.display = "none";
+    validationPopUp.style.display = "block"
+    modalbg.style.display = "none"
   }
 })
 
 closeBtn.addEventListener("click", function(){
-  validationPopUp.style.display = "none";
-  document.getElementById("my-form").reset();
+  validationPopUp.style.display = "none"
+  document.getElementById("my-form").reset()
+  document.location.reload()
 })
 closeCross.addEventListener("click", function(){
-  validationPopUp.style.display = "none";
-  document.getElementById("my-form").reset();
-
+  validationPopUp.style.display = "none"
+  document.getElementById("my-form").reset()
+  document.location.reload()
 })
